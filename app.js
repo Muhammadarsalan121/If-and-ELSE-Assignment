69598208b771071d09a0c2079634cbe555eba4f8
unknown <arsalanshahid486@gmail.com>
2024-03-01 00:19:26 +0500

    Assignment done

---
+++ app.js
@@ -0,0 +1,47 @@
+        //   NUMBER 1
+// var number = +prompt("Enter the number");
+// if (number % 3 === 0){
+//         console.log("It is Divisible by 3")
+// }
+// else {
+//         console.log("It is not divisible by 3")
+// }
+
+
+        //   NUMBER 2
+// var percentage = +prompt("enter your percentage")
+
+// if(percentage  >=91 && percentage <=  100 ){
+//     console.log(" A1 grade")
+    
+// }
+
+//  else if(percentage  >=81 &&  percentage <= 90 ){
+//     console.log("A+ grade")
+
+// }
+
+//  else if(percentage  >=71 &&  percentage <= 80 ){
+//     console.log("A grade")
+
+// }
+
+//  else if(percentage  >=61 &&  percentage <= 70 ){
+//     console.log("B grade")
+
+// }
+
+//  else if(percentage  >=51 &&  percentage <= 60 ){
+//     console.log(" grade")
+
+// }
+
+            //   NUMBER 3
+
+// var age = +prompt("Enter your age")
+// if(age  > 18 ){
+//         console.log(" OLD Enough")
+// }
+// else{
+//     console.log("Too young")
+// }
