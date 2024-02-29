83a22b5d6a90ceafebf4de660828d7d203532697
unknown <arsalanshahid486@gmail.com>
2024-03-01 00:36:20 +0500

    Assignment craeted

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
         //   NUMBER 1
 // var number = +prompt("Enter the number");
 // if (number % 3 === 0){
-//         console.log("It is Divisible by 3")
+//         console.log("It is Divisible by 3");
 // }
 // else {
-//         console.log("It is not divisible by 3")
+//         console.log("It is not divisible by 3"); 
 // }
 
 
@@ -12,36 +12,67 @@
 // var percentage = +prompt("enter your percentage")
 
 // if(percentage  >=91 && percentage <=  100 ){
-//     console.log(" A1 grade")
+//     console.log(" A1 grade"); 
     
 // }
 
 //  else if(percentage  >=81 &&  percentage <= 90 ){
-//     console.log("A+ grade")
+//     console.log("A+ grade"); 
 
 // }
 
 //  else if(percentage  >=71 &&  percentage <= 80 ){
-//     console.log("A grade")
+//     console.log("A grade"); 
 
 // }
 
 //  else if(percentage  >=61 &&  percentage <= 70 ){
-//     console.log("B grade")
+//     console.log("B grade"); 
 
 // }
 
 //  else if(percentage  >=51 &&  percentage <= 60 ){
-//     console.log(" grade")
+//     console.log(" grade"); 
 
 // }
 
             //   NUMBER 3
 
-// var age = +prompt("Enter your age")
+// var age = +prompt("Enter your age"); 
 // if(age  > 18 ){
-//         console.log(" OLD Enough")
+//         console.log(" OLD Enough"); 
 // }
 // else{
-//     console.log("Too young")
+//     console.log("Too young"); 
+// }
+
+
+
+        // NUMBER 4
+
+        // var number = +prompt("Enter the number");
+// if (number % 2 === 0){
+//         console.log("It is even number"); 
+// }
+// else {
+//         console.log("It is odd number"); 
+// }
+
+        //  NUMBER 5
+
+// var time = prompt("Enter the time"); 
+// if(time >= 0000 && time < 1200){
+//     console.log("Good Morning")
+// }   
+// else if(time >= 1200 && time < 1700){
+//     console.log("Good Afternoon")
+// } 
+// else if(time >= 1700 && time < 2100){
+//     console.log("Good Evening")
+// }
+// else if(time >= 2100 && time < 2359){
+//     console.log("Good Night")
+// }
+// else{
+//     console.log("Next day started")
 // }
